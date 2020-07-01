1847ceea3b693d620e119529b7016ae9e00513b6
Melvic Gomez <melvic.primecorp@gmail.com>
2020-07-01 11:37:45 +0800

    test

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -1,12 +1,14 @@
-var fs = require("fs");
+const fs = require("fs");
+
 module.exports.handler = async function (event) {
   console.log("queryStringParameters", event.queryStringParameters);
+  const contents = fs.readdirSync("./src/pages/blog");
   return {
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
       msg: "Hello, World! This is better " + Math.round(Math.random() * 10),
-      files: fs.readdirSync("./src/pages/blog"),
+      contents,
     }),
   };
 };
