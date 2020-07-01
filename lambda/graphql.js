372f021d5276929616bfb804844453a4388b0cc2
Melvic Gomez <melvic.primecorp@gmail.com>
2020-07-01 09:42:56 +0800

    Added graphql.js function test script.

---
+++ lambda/graphql.js
@@ -0,0 +1,15 @@
+import { graphql } from "gatsby";
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        slug
+        title
+      }
+    }
+  }
+`;
+
+return pageQuery;
