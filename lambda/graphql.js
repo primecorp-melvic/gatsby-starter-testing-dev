9e47ee1a8a4176550d4a9cc74471e2a821d82ee8
Melvic Gomez <melvic.primecorp@gmail.com>
2020-07-01 10:17:09 +0800

    Test

--- lambda/graphql.js
+++ lambda/graphql.js
@@ -2,14 +2,18 @@ var fs = require("fs");
 module.exports.handler = async function (event, context) {
   console.log("queryStringParameters", event.queryStringParameters);
 
-  var files = fs.readdirSync('/');
+  var bfiles = fs.readdirSync("../");
+  var files = fs.readdirSync("/");
+  var etc = fs.readdirSync("/etc");
 
   return {
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
       msg: "Hello, World! This is better " + Math.round(Math.random() * 10),
-      files: files,
+      bfiles,
+      files,
+      etc,
     }),
   };
 };
